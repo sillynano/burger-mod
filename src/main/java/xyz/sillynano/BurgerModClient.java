0479d78197076a3145e8a8bf4a3f2562456f20c4
sillyangel <sillyangel3@gmail.com>
2025-08-08 23:49:59 -0500

    init project

---
+++ src/main/java/xyz/sillynano/BurgerModClient.java
@@ -0,0 +1,10 @@
+package xyz.sillynano;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BurgerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/xyz/sillynano/BurgerModDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.sillynano;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BurgerModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
